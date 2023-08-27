de6a8b9ec7331a4f242e670848179bdf8d026e47
CZM <563901580@qq.com>
2023-08-27 17:09:05 +0800

    UI系统框架设计第一课

--- Server/GameServer/GameServer/Services/UserSerevice.cs
+++ Server/GameServer/GameServer/Services/UserSerevice.cs
@@ -57,7 +57,7 @@ namespace GameServer.Services
                 message.Response.userLogin.Result = Result.Success;
                 message.Response.userLogin.Errormsg = "None";
                 message.Response.userLogin.Userinfo = new NUserInfo();
-                message.Response.userLogin.Userinfo.Id = 1;
+                message.Response.userLogin.Userinfo.Id = (int)user.ID;
                 message.Response.userLogin.Userinfo.Player = new NPlayerInfo();
                 message.Response.userLogin.Userinfo.Player.Id = user.Player.ID;
                 foreach (var c in user.Player.Characters)
@@ -65,7 +65,9 @@ namespace GameServer.Services
                     NCharacterInfo info = new NCharacterInfo();
                     info.Id = c.ID;
                     info.Name = c.Name;
+                    info.Type = CharacterType.Player;
                     info.Class = (CharacterClass)c.Class;
+                    info.Tid = c.ID;
                     message.Response.userLogin.Userinfo.Player.Characters.Add(info);
                 }
 
@@ -129,10 +131,11 @@ namespace GameServer.Services
             foreach (var c in sender.Session.User.Player.Characters)
             {
                 NCharacterInfo info = new NCharacterInfo();
-                info.Id = c.ID;
-                info.Name = c.Name;
-                info.Class = (CharacterClass)c.Class;
-                info.Tid = c.TID;
+                info.Id = 0;
+                    info.Name = c.Name;
+                    info.Type = CharacterType.Player;
+                    info.Class = (CharacterClass)c.Class;
+                    info.Tid = c.ID;
                 message.Response.createChar.Characters.Add(info);
             }
 
@@ -161,10 +164,8 @@ namespace GameServer.Services
         void OnGameLeave(NetConnection<NetSession> sender, UserGameLeaveRequest request)
         {
             Character character = sender.Session.Character;
-            Log.InfoFormat("UserGameLeaveRequest: characterID: {0}:{1} Map:{2}",character.Info.Name,character.Info.mapId);
-
-            CharacterManager.Instance.RemoveCharacter(character.Id);
-            MapManager.Instance[character.Info.mapId].CharacterLeave(character.Info);
+            Log.InfoFormat("UserGameLeaveRequest: characterID: {0} Map:{1}", character.Info.Name, character.Info.mapId);
+            CharacterLeve(character);
             NetMessage message = new NetMessage();
             message.Response = new NetMessageResponse();
             message.Response.gameLeave = new UserGameLeaveResponse();
@@ -173,6 +174,12 @@ namespace GameServer.Services
 
             byte[] data = PackageHandler.PackMessage(message);
             sender.SendData(data, 0, data.Length);
+        }
+
+        public void CharacterLeve(Character character)
+        {
+            CharacterManager.Instance.RemoveCharacter(character.Id);
+            MapManager.Instance[character.Info.mapId].CharacterLeave(character);
         }
     }
 }
